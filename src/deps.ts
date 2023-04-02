1c2096a2872cb2ec4cb5e0ac27a4daffd5a1b645
mys1024 <mys.1024@outlook.com>
2023-04-02 18:45:23 +0800

    chore: update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
-export * as colors from "https://deno.land/std@0.178.0/fmt/colors.ts";
+export * as colors from "https://deno.land/std@0.182.0/fmt/colors.ts";
 
-export * as oak from "https://deno.land/x/oak@v12.0.0/mod.ts";
+export * as oak from "https://deno.land/x/oak@v12.1.0/mod.ts";
 
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
